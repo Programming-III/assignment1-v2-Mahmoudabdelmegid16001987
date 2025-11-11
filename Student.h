35180979951630b3c33ff901a0eaefe451a89e43
Mahmoud abdelmegid 16001987 <mahmoudwaelsamir19@gmail.com>
2025-11-11 09:44:33 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,11 +1,33 @@
-#ifndef STUDENT_H
-#define STUDENT_H
+//
+// Created by Mahmoud Wael on 11/11/2025.
+//
 
-#include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
+#ifndef UNTITLED2_STUDENT_H
+#define UNTITLED2_STUDENT_H
 
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
+
+public:
+    Student();
+    Student(int yl , string m);
+    ~Student();
+   void displayinfo();
+    int getyearLevel() {
+        return yearLevel;
+    }
+    string getmajor() {
+        return major;
+    }
+
+
+};
+
+
+
+#endif //UNTITLED2_STUDENT_H
 
 
 
