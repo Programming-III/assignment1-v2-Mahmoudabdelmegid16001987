1e28043f940f5036bd60648ffcbb99ffe6857341
Mahmoud abdelmegid 16001987 <mahmoudwaelsamir19@gmail.com>
2025-11-11 09:55:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,64 @@
 #include <iostream>
-#include <string>
-#include "Person.h"
-#include "Student.h"
-#include "Instructor.h"
-#include "Course.h"
+#include"Person.h"
+#include"Student.h"
+#include"Course.h"
 
+Person :: Person() {
 
-using namespace std;
-
-// ==================== Person Class Implementation =========================
-
-
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
-    return 0;
 }
+Person ::Person(string n , int idd) {
+    name = n;
+    id = idd;
+}
+~Person() {
+
+}
+void Person :: display() {
+    cout << "Person Name :" << getName() << endl;
+    cout << "Person Id :" << getId() << endl;
+}
+
+Student()
+{
+}
+~Student ()
+{
+   delete [] students;
+}
+void Student :: displayinfo(): display()
+{
+    cout << " Student yearLevel :" << getyearLevel() << endl;
+    cout << " Student major : " << getmajor() << endl;
+}
+
+
+
+ void Course :: addStudent(const Student& s) {
+    Student* newStudents = new Student[currentStudents ++];
+for (int i =0; i<currentStudents;i++) {
+    newStudents[i]= students[i];
+}
+    currentStudents++;
+    students = newStudents;
+    delete [] newStudents;
+
+
+
+
+}
+
+Course()
+{
+}
+~Course()
+{
+}
+
+void Course :: displayCourseInfo() {
+    
+}
+
+int main() {
+
+}
+    return 0;
