a173eed6fafbe7d72be641725a93782915198d4f
Mahmoud abdelmegid 16001987 <mahmoudwaelsamir19@gmail.com>
2025-11-11 09:42:50 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,10 +1,35 @@
-#ifndef COURSE_H
-#define COURSE_H
+//
+// Created by Mahmoud Wael on 11/11/2025.
+//
 
-#include <string>
-#include "Student.h"
-using namespace std;
-//#write your code here
+#ifndef UNTITLED2_COURSE_H
+#define UNTITLED2_COURSE_H
+
+class Course : public Student{
+private:
+ string courseCode;
+ string courseName;
+ int maxStudents;
+ Student* students;
+ int currentStudents;
+
+public:
+    Course ();
+    Course (string CC, string cN, int Ms, Student* s, int cs);
+    ~Course();
+  void addStudent(const Student& s);
+    void displayCourseInfo();
+
+
+
+
+
+};
+
+
+
+
+#endif //UNTITLED2_COURSE_H
 
 
 
